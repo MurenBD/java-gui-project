c87e0dc252d3a2dce6e440c722dad20a575dd0ed
murentheblackdragon <murentheblackdragon@gmail.com>
2020-11-26 16:42:46 +0800

    eod push

---
+++ src/main/MainController.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    TextField xTextField;
+
+    @FXML
+    TextField yTextField;
+
+    @FXML
+    Button coordButton;
+
+    @FXML
+    ListView<String> xyList;
+
+
+}
