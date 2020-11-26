f5b3e62752e8258c74e9719a6ab39617908770cd
murentheblackdragon <murentheblackdragon@gmail.com>
2020-11-26 16:43:54 +0800

    updated controller code

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Main extends Application {
--- src/main/MainController.java
+++ src/main/MainController.java
@@ -1,22 +1,73 @@
 package main;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.scene.text.Text;
 
-public class Controller {
+public class MainController {
+    //Variables
+    int dbx, dby;
+
+    //Dashboard Components
     @FXML
-    TextField xTextField;
+    TextField dbxTextField;
 
     @FXML
-    TextField yTextField;
+    TextField dbyTextField;
 
     @FXML
-    Button coordButton;
+    Button dbcoordButton;
 
     @FXML
-    ListView<String> xyList;
+    ListView<String> dbxyListView;
 
+    @FXML
+    LineChart dbxyChart;
+    XYChart.Series dataseries;
+
+    @FXML
+    NumberAxis dbxAxis;
+
+    @FXML
+    NumberAxis dbyAxis;
+
+    //Connection Components
+
+    @FXML
+    TextField cConnectionStatusTextField;
+
+    @FXML
+    TextField cAddressTextField;
+
+    @FXML
+    Button cConnectButton;
+
+    public MainController(){
+        initialize();
+    }
+
+    private void initialize(){
+        //TextFormatter numberFormat = new TextFormatter();
+        //dbxTextField.setTextFormatter(numberFormat);
+
+        //init dbxyChart
+        //dbxAxis = new NumberAxis();
+        //dbyAxis = new NumberAxis();
+        //dbxyChart = new LineChart(dbxAxis, dbyAxis);
+       // dataseries = new XYChart.Series();
+    }
+
+
+    @FXML
+    public void coordButtonClicked(Event e){
+        dataseries.getData().add(new XYChart.Data());
+    }
 
 }
